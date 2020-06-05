dee66949f4404ca20529083bf776841a8ddd1d80
Ildana <ildana.ruzybayeva@gmail.com>
2020-06-05 17:06:49 +0000

    Add testsing  with Jasmine to syllabus

---
+++ JasmineTests/task.txt
@@ -0,0 +1,12 @@
+1) Create a new instance of Person and call it student
+Insert values according to the constructor 
+Copy the test and adjust it so that it won't fail
+Play around with it until you get it
+
+2) Create a new class of any object that comes 
+to your mind (like car, house, furniture etc)
+Add a constructor 
+Create a new instance of the class that you created
+Write the tests using the following Matchers (
+    toContain, toEqual, toBeLessThan, toBeDefined, not.toBe
+)
---
+++ JasmineTests/test.js
@@ -0,0 +1,23 @@
+class Person {
+  constructor(name, age, hobbies, isVegan){
+    this.name = name
+    this.age = age
+    this.hobbies = hobbies
+    this.isVegan = isVegan
+  }
+}
+
+const teacher = new Person("Ildana", 24, ['coding', 'gaming'], true)  
+
+  describe("Teacher", function(){
+    it("Is of legal age", function(){
+      expect(teacher.age).toBeGreaterThan(18)
+    });
+    it("Likes coding", function(){
+      expect(teacher.hobbies).toContain('coding')
+    });
+    it("Does not eat meat", function(){
+      expect(teacher.isVegan).toBe(true)
+    });
+  }); 
+
