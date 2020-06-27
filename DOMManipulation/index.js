f21b5afbe3fc64584d0a3870659f896e82f6cf77
ildanaruzybayeva <ildana.ruzybayeva@gmail.com>
2020-06-27 13:48:07 +0200

    switch between eng and rus

--- DOMManipulation/index.js
+++ DOMManipulation/index.js
@@ -1,37 +1,45 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
+const btnEng = document.querySelector('.start-en')
+const btnRus = document.querySelector('.start-ru')
+const btnEnd = document.querySelector('.end')
+
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
-recognition.lang = 'en-US';
 
 let p = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(p);
 
-const btnStart = document.querySelector('.start')
-const btnEnd = document.querySelector('.end')
+const handleStart = (e) => {
+    recognition.start();
+    recognition.lang = e.target.id
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('');
 
-recognition.addEventListener('result', e => {
-    const transcript = Array.from(e.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
-        .join('');
+        p.textContent = transcript.replace(/poop|poo|shit|dump/gi, '💩')
+            .replace(/heart|love|nice/gi, '❤️ ')
 
-    const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
-    p.textContent = poopScript;
+        // if (e.results[0].isFinal) {
+        //     p = document.createElement('p')
+        //     words.appendChild(p);
+        // }
+    });
+    recognition.addEventListener('end', recognition.stop);
+}
 
-    if (e.results[0].isFinal) {
-        p = document.createElement('p');
-        words.appendChild(p);
-    }
-});
 
-recognition.addEventListener('end', recognition.start);
-recognition.start();
-
-const handleStop = () => {
+const handleStop = (e) => {
     recognition.stop();
     console.log('Speech recognition has stopped.');
 }
 
 btnEnd.addEventListener('click', handleStop)
+btnEng.addEventListener('click', handleStart)
+btnRus.addEventListener('click', handleStart)
+
+
+
